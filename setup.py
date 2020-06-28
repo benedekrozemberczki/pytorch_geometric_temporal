fbf088b78760fb416442d009710b43e7d9762187
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-28 23:59:53 +0000

    Test coverage

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import find_packages, setup
 
 install_requires = ["torch",
+                    "torch_sparse",
+                    "torch_scatter",
+                    "torch_cluster", 
+                    "torch_spline_conv",
                     "torch_geometric",
                     "numpy",
                     "networkx",
