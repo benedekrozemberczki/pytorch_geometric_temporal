16253549214e6e0499ca28b65c136b37ac29ecd8
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-28 23:54:24 +0000

    Test coverage

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
 
 install_requires = ["torch",
+                    "torch_geometric",
                     "numpy",
                     "networkx",
                     "scipy",
