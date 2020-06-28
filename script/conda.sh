0dd1262273f9fc439bfda0aae2dc8e7861a479cc
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-28 23:37:35 +0000

    Test coverage

--- script/conda.sh
+++ script/conda.sh
@@ -7,13 +7,6 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
   PATH=/home/travis/miniconda3/bin:${PATH}
 fi
 
-if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-  wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
-  chmod +x miniconda.sh
-  ./miniconda.sh -b
-  PATH=/Users/travis/miniconda3/bin:${PATH}
-fi
-
 conda update --yes conda
 
 conda create --yes -n test python="${PYTHON_VERSION}"
