8fc6a7999bea938381bf213a10a48876a04a523f
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-29 01:53:23 +0000

    Test coverage

--- test/general_test.py
+++ test/general_test.py
@@ -5,7 +5,7 @@ def test_gconv_lstm_layer():
     Testing the Dummy Layer.
     """
 
-    layer = GConvLSTM(in_channels=32, out_channels=64, K=5, number_of_nodes=100)
+    layer = GConvLSTM(in_channels=32, out_channels=64, K=2, number_of_nodes=100)
 
     assert layer.in_channels == 32
     assert layer.out_channels == 64
