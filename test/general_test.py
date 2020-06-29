5c376ad38e0e87f57f8f015b020bc4a22021f9e5
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-29 13:52:31 +0000

    GConv LSTM

--- test/general_test.py
+++ test/general_test.py
@@ -1,11 +1,19 @@
+import torch
+import numpy as np
+import networkx as nx
 from torch_geometric_temporal.nn.conv import GConvLSTM
+
+
 
 def test_gconv_lstm_layer():
     """
     Testing the GConvLSTM Layer.
     """
 
-    layer = GConvLSTM(in_channels=32, out_channels=64, K=2, number_of_nodes=100)
+    graph = nx.watts_strogatz_graph(100, 10, 0.5)
+    features = torch.FloatTensor(np.random.uniform(-1, 1, (100, 64)))
 
-    assert layer.in_channels == 32
-    assert layer.out_channels == 64
+    layer = GConvLSTM(in_channels=64, out_channels=16, K=2, number_of_nodes=100)
+
+    assert layer.in_channels == 64
+    assert layer.out_channels == 16
