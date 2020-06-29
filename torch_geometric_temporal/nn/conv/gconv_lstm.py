b4f9b107fac151aa15612ff12bab59d0c0897dbd
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-29 00:57:20 +0000

    Test coverage

--- torch_geometric_temporal/nn/conv/gconv_lstm.py
+++ torch_geometric_temporal/nn/conv/gconv_lstm.py
@@ -6,6 +6,26 @@ from torch_geometric.nn.inits import glorot, zeros
 
 class GConvLSTM(object):
 
-    def __init__(self, in_channels, out_channels):
-        self.in_channels = in_channels
+    def __init__(self, feature_in_channels, hidden_in_channels, out_channels):
+        self.feature_in_channels = feature_in_channels
+        self.hidden_in_channels = hidden_in_channels
         self.out_channels = out_channels
+
+
+    def reset_parameters():
+
+    def set_hidden_state(self, X, H):
+        if H is None:
+            H = torch.zeros(X.shape[0], self.out_channels)
+        return H
+         
+
+    def set_cell_state(self, X, c):
+        if c is None:
+            c = torch.Tensor(1, self.out_channels)
+        return c
+
+    def __call__(self, X, edge_index, edge_weight=None, H=None, c=None):
+        c = self.set_cell_state(X, c)
+        H = self.set_hidden_state(X, H)
+    
