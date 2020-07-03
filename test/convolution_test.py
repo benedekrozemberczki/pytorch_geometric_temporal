aa07e6fa3a64794ce5f2b8ff536bbf73f11e31a4
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-03 00:57:31 +0000

    LRGCN

--- test/convolution_test.py
+++ test/convolution_test.py
@@ -5,6 +5,9 @@ from torch_geometric_temporal.nn.conv import GConvLSTM, GConvGRU, GCLSTM, LRGCN
 
 
 def create_mock_data(number_of_nodes, edge_per_node, in_channels):
+    """
+    Creating a mock feature matrix and edge index.
+    """
     graph = nx.watts_strogatz_graph(number_of_nodes, edge_per_node, 0.5)
     edge_index = torch.LongTensor(np.array([edge for edge in graph.edges()]).T)
     X = torch.FloatTensor(np.random.uniform(-1, 1, (number_of_nodes, in_channels)))
@@ -12,15 +15,25 @@ def create_mock_data(number_of_nodes, edge_per_node, in_channels):
 
 
 def create_mock_states(number_of_nodes, out_channels):
+    """
+    Creating mock hidden and cell states. 
+    """
     H = torch.FloatTensor(np.random.uniform(-1, 1, (number_of_nodes, in_channels)))
     C = torch.FloatTensor(np.random.uniform(-1, 1, (number_of_nodes, in_channels)))
     return H, C
 
 
 def create_mock_edge_weight(edge_index):
+    """
+    Creating a mock edge weight tensor.
+    """
     return torch.FloatTensor(np.random.uniform(0, 1, (edge_index.shape[1])))
 
+
 def create_mock_edge_relations(edge_index, num_relations):
+    """
+    Creating a mock relation type tensor.
+    """
     return torch.LongTensor(np.random.choice(num_relations, edge_index.shape[1], replace=True))
 
 
