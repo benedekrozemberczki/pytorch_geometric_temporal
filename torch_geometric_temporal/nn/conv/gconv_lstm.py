325820d090f9774edb56c72318ef135946ae0999
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-19 02:19:54 +0100

    PyTorch Geometric Temporal

--- torch_geometric_temporal/nn/conv/gconv_lstm.py
+++ torch_geometric_temporal/nn/conv/gconv_lstm.py
@@ -148,7 +148,8 @@ class GConvLSTM(torch.nn.Module):
         return H
 
 
-    def forward(self, X, edge_index, edge_weight=None, H=None, C=None):
+    def forward(self, X: torch.FloatTensor, edge_index: torch.LongTensor,
+                edge_weight: torch.FloatTensor=None, H: torch.FloatTensor=None, C: torch.FloatTensor=None):
         """
         Making a forward pass. If edge weights are not present the forward pass
         defaults to an unweighted graph. If the hidden state and cell state
