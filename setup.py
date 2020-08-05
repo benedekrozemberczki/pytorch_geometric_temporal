4f9c20e78606b30de4a58b46bebf2eafd8faf3e2
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-05 22:48:54 +0000

    Fixes

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
   install_requires = install_requires,
   setup_requires = setup_requires,
   tests_require = tests_require,
-  python_requires='>=3.6',
+  python_requires = '>=3.6',
   classifiers = ["Development Status :: 3 - Alpha",
                  "Intended Audience :: Developers",
                  "Topic :: Software Development :: Build Tools",
