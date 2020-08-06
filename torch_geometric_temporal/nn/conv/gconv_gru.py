3fdf71055b09cd12ebc623ba460dd43e34bf5ce6
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-06 15:04:53 +0000

    Comments on DyGrEn

--- torch_geometric_temporal/nn/conv/gconv_gru.py
+++ torch_geometric_temporal/nn/conv/gconv_gru.py
@@ -102,8 +102,8 @@ class GConvGRU(torch.nn.Module):
         Arg types:
             * **X** *(PyTorch Float Tensor)* - Node features.
             * **edge_index** *(PyTorch Long Tensor)* - Graph edge indices.
-            * **edge_weight** *(PyTorch Long Tensor)* - Edge weight vector (optional).
-            * **H** *(PyTorch Float Tensor)* - Hidden state matrix for all nodes (optional).
+            * **edge_weight** *(PyTorch Long Tensor, optional)* - Edge weight vector.
+            * **H** *(PyTorch Float Tensor, optional)* - Hidden state matrix for all nodes.
 
         Return types:
             * **H** *(PyTorch Float Tensor)* - Hidden state matrix for all nodes.
