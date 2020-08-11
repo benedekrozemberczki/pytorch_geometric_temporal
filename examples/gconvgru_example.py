29b766e936e73052f0184176e340fcb3cc6d5faa
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-11 17:19:26 +0000

    GConvGRU Example

--- examples/gconvgru_example.py
+++ examples/gconvgru_example.py
@@ -1,5 +1,22 @@
 import torch
+import torch.nn.functional as F
+from torch_geometric_temporal.nn.conv import GConvGRU
 
-from torch.nn import Linear as Lin, ReLU
+class Net(torch.nn.Module):
+    def __init__(self, node_features, num_classes):
+        super(Net, self).__init__()
+        self.recurrent_1 = GConvGRU(node_features, 32)
+        self.recurrent_2 = GConvGRU(32, 16)
+        self.linear = torch.nn.Linear(16, num_classes)
+
+    def forward(self, x, edge_index, edge_weight):
+        x = self.recurrent_1(x, edge_index, edge_weight)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.recurrent_2(x, edge_index, edge_weight)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.linear(x)
+        return F.log_softmax(x, dim=1)
 
 
