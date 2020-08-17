0348141221c641b1288bb69461c401851c9a55ad
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-17 16:22:01 +0000

    Evolve GCN H

--- test/convolution_test.py
+++ test/convolution_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import networkx as nx
 from torch_geometric_temporal.nn.conv import GConvLSTM, GConvGRU
 from torch_geometric_temporal.nn.conv import GCLSTM, LRGCN, DyGrEncoder
-
+from torch_geometric_temporal.nn.conv import EvolveGCNH
 
 def create_mock_data(number_of_nodes, edge_per_node, in_channels):
     """
