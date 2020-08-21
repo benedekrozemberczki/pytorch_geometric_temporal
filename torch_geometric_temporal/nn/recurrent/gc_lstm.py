c8749d6ccdf704334c28a322e9209713883f5fe3
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-21 15:11:56 +0000

    PTG

--- torch_geometric_temporal/nn/recurrent/gc_lstm.py
+++ torch_geometric_temporal/nn/recurrent/gc_lstm.py
@@ -53,7 +53,9 @@ class GCLSTM(torch.nn.Module):
 
         self.conv_i = ChebConv(in_channels=self.out_channels,
                                out_channels=self.out_channels,
-                               K=self.K)
+                               K=self.K,
+                               normalization=self.normalization,
+                               bias=self.bias)
 
         self.W_i = Parameter(torch.Tensor(self.in_channels, self.out_channels))
         self.b_i = Parameter(torch.Tensor(1, self.out_channels))
