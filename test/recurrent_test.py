aaa1718ec376c4e56053969436a9a043e919ad0b
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-27 19:48:57 +0000

    [TEST] Add corrected DyGRAE test.

--- test/recurrent_test.py
+++ test/recurrent_test.py
@@ -201,8 +201,8 @@ def test_dygrencoder_layer():
     H_tilde, H, C = layer(X, edge_index, edge_weight, H, C)
 
     assert H_tilde.shape == (number_of_nodes, lstm_out_channels)
-    assert C.shape == (number_of_nodes, lstm_out_channels)
     assert H.shape == (number_of_nodes, lstm_out_channels)
+    assert C.shape == (number_of_nodes, lstm_out_channels)
 
 def test_evolve_gcn_h_layer():
     """
