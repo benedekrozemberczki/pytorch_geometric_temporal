6faeb7c44ee276952d1d3a7dfe9d9accc36a27a8
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-27 20:04:31 +0000

    [EXAMPLE] Add corrected DyGRAE Example.

--- torch_geometric_temporal/nn/recurrent/dygrae.py
+++ torch_geometric_temporal/nn/recurrent/dygrae.py
@@ -15,7 +15,7 @@ class DyGrEncoder(torch.nn.Module):
         lstm_out_channels (int): Number of LSTM channels.
         lstm_num_layers (int): Number of neurons in LSTM.
     """
-    def __init__(self, conv_out_channels: int, conv_num_layers: int, conv_aggr: str="mean",
+    def __init__(self, conv_out_channels: int, conv_num_layers: int, conv_aggr: str,
                  lstm_out_channels: int, lstm_num_layers: int):
         super(DyGrEncoder, self).__init__()
         assert conv_aggr in ["mean", "add", "max"], "Wrong aggregator."
