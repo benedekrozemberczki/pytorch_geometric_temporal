287138e1b39465a82f2a6a5bd3f4561044237172
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-27 19:52:35 +0000

    [TEST] Add corrected DyGRAE test.

--- examples/dygrae_example.py
+++ examples/dygrae_example.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import networkx as nx
 import torch.nn.functional as F
-from torch_geometric_temporal.nn.recurrent import GCLSTM
+from torch_geometric_temporal.nn.recurrent import DyGrEncoder
 
 
 def create_mock_data(number_of_nodes, edge_per_node, in_channels):
@@ -32,9 +32,9 @@ def create_mock_target(number_of_nodes, number_of_classes):
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features, num_classes):
         super(RecurrentGCN, self).__init__()
-        self.recurrent_1 = GCLSTM(node_features, 32, 5)
-        self.recurrent_2 = GCLSTM(32, 16, 5)
-        self.linear = torch.nn.Linear(16, num_classes)
+        self.recurrent_1 = DyGrEncoder(node_features, 32, 5)
+        self.recurrent_2 = DyGrEncoder(32, 32, 5)
+        self.linear = torch.nn.Linear(32, num_classes)
 
     def forward(self, x, edge_index, edge_weight):
         x, _ = self.recurrent_1(x, edge_index, edge_weight)
