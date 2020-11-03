fba652012f433fc16a9a804c30c315f1ded29439
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-03 23:23:09 +0000

    PTG

--- torch_geometric_temporal/data/discrete/temporal_signal_static_graph.py
+++ torch_geometric_temporal/data/discrete/temporal_signal_static_graph.py
@@ -9,7 +9,6 @@ class StaticGraphDiscreteTemporalSignal(object):
         self._features = features
         self._targets = targets
 
-
     def _get_edge_index(self):
         pass
 
@@ -22,14 +21,14 @@ class StaticGraphDiscreteTemporalSignal(object):
     def _get_target(self):
         pass
 
+    def _generate_snapshot(self)
+        pass
+
     def __next__(self):
         self.t = t + 1
         if self.t <= len(self.features):
-            edge_index = self._get_edge_index()
-            edge_weight = self._get_edge_weight()
-            features = self._get_features()
-            target = self._get_targe[t] 
-            return (self.edges, self.edge_weight, feature, target)
+            snapshot = self._generate_snapshot()
+            return snapshot
         else:
             raise StopIteration
 
