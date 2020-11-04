326af02df9c02d8bc00e37a39f35ebb48cf49891
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-04 00:09:18 +0000

    Torch Geometric Temporal Discrete

--- torch_geometric_temporal/data/discrete/temporal_signal_static_graph.py
+++ torch_geometric_temporal/data/discrete/temporal_signal_static_graph.py
@@ -11,46 +11,36 @@ class StaticGraphDiscreteTemporalSignal(object):
         self._targets = targets
 
     def _get_edge_index(self):
-        if self._edge_index is None:
-            return self._edge_index
-        else:
-            return torch.LongTensor(self._edge_index)
+        return torch.LongTensor(self._edge_index)
 
     def _get_edge_attr(self):
-        if self._edge_attr is None:
-            return self._edge_attr
-        else:
-            return torch.FloatTensor(self._edge_attr)
+        return torch.FloatTensor(self._edge_attr)
 
-    def _get_features(self):
-        if self._features[t] is None:
-            return self._features[t]
-        else:        
-            return torch.FloatTensor(self._features[t])
+    def _get_features(self):        
+        return torch.FloatTensor(self._features[t])
 
     def _get_target(self):
-        if self._targets[t] is None:
-           return self._targets[t]
-        else:
-            if self.targets[t].dtype.kind == 'i':
-                return torch.LongTensor(self._targets[t])
-            elif self.targets[t].dtype.kind == 'f':
-                return torch.FloatTensor(self._targets[t])
+        if self.targets[t].dtype.kind == 'i':
+            return torch.LongTensor(self._targets[t])
+        elif self.targets[t].dtype.kind == 'f':
+            return torch.FloatTensor(self._targets[t])
          
 
-    def _generate_snapshot(self):
+    def _get_snapshot(self):
         x = self._get_features()
         edge_index = self._get_edge_index()
         edge_attr = self._get_edge_attr()
         y = self._get_target()
 
-        snapshot = Data(x = x, edge_index = edge_index,
-                        edge_attr = edge_attr, y = y)
+        snapshot = Data(x = x,
+                        edge_index = edge_index,
+                        edge_attr = edge_attr,
+                        y = y)
 
     def __next__(self):
         self.t = t + 1
         if self.t <= len(self.features):
-            snapshot = self._generate_snapshot()
+            snapshot = self._get_snapshot()
             return snapshot
         else:
             raise StopIteration
