c85ea6696bfce3b880e7048427b10a0efe4411ab
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-08 20:05:01 +0000

    PTG

--- torch_geometric_temporal/data/dataset/chickenpox.py
+++ torch_geometric_temporal/data/dataset/chickenpox.py
@@ -6,4 +6,10 @@ class ChickenpoxDataset(object):
 
     def __init__(self):
         return self
+
+    def _read_web_data(self):
+        url = "https://raw.githubusercontent.com/benedekrozemberczki/pytorch_geometric_temporal/master/dataset/discrete/chickenpox.json"
+        dataset = json.loads(urllib.request.urlopen(url).read())
+        
+ 
         
