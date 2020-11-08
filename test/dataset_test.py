d403b6c85c420fe984b1f9073e70e6e9378b1067
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-08 21:50:42 +0000

    PTG

--- test/dataset_test.py
+++ test/dataset_test.py
@@ -5,3 +5,6 @@ def test_chickenpox():
     loader = ChickenpoxDatasetLoader()
     dataset = loader.get_dataset()
     assert dataset.edge_index.shape == (2, 102)
+    for epoch in range(10):
+        for snapshot in dataset:
+            assert snapshot.edge_index.shape ==(2, 102)
--- torch_geometric_temporal/data/discrete/static_graph_discrete_signal.py
+++ torch_geometric_temporal/data/discrete/static_graph_discrete_signal.py
@@ -4,57 +4,59 @@ from torch_geometric.data import Data
 
 class StaticGraphDiscreteSignal(object):
 
-    def __init__(self, edge_index, edge_attr, features, targets):
+    def __init__(self, edge_index, edge_weight, features, targets):
         self.edge_index = edge_index
-        self.edge_attr = edge_attr
+        self.edge_weight = edge_weight
         self.features = features
         self.targets = targets
 
     def _get_edge_index(self):
-        if self._edge_index is None:
-            return self._edge_index
+        if self.edge_index is None:
+            return self.edge_index
         else:
-            return torch.LongTensor(self._edge_index)
+            return torch.LongTensor(self.edge_index)
 
-    def _get_edge_attr(self):
-        if self._edge_attr is None:
-            return self._edge_attr
+    def _get_edge_weight(self):
+        if self.edge_weight is None:
+            return self.edge_weight
         else:
-            return torch.FloatTensor(self._edge_attr)
+            return torch.FloatTensor(self.edge_weight)
 
     def _get_features(self): 
-        if self._features[t]:
-            return self._features[t]
+        if self.features[self.t] is None:
+            return self.features[self.t]
         else:       
-            return torch.FloatTensor(self._features[t])
+            return torch.FloatTensor(self.features[self.t])
 
     def _get_target(self):
-        if self.targets[t] is None:
-            return self.targets[t]
+        if self.targets[self.t] is None:
+            return self.targets[self.t]
         else:
-            if self.targets[t].dtype.kind == 'i':
-                return torch.LongTensor(self._targets[t])
-            elif self.targets[t].dtype.kind == 'f':
-                return torch.FloatTensor(self._targets[t])
+            if self.targets[self.t].dtype.kind == 'i':
+                return torch.LongTensor(self.targets[self.t])
+            elif self.targets[self.t].dtype.kind == 'f':
+                return torch.FloatTensor(self.targets[self.t])
          
 
     def _get_snapshot(self):
         x = self._get_features()
         edge_index = self._get_edge_index()
-        edge_attr = self._get_edge_attr()
+        edge_weight = self._get_edge_weight()
         y = self._get_target()
 
         snapshot = Data(x = x,
                         edge_index = edge_index,
-                        edge_attr = edge_attr,
+                        edge_attr = edge_weight,
                         y = y)
+        return snapshot
 
     def __next__(self):
-        self.t = t + 1
-        if self.t <= len(self.features):
+        if self.t < len(self.features):
             snapshot = self._get_snapshot()
+            self.t = self.t + 1
             return snapshot
         else:
+            self.t = 0
             raise StopIteration
 
     def __iter__(self):
