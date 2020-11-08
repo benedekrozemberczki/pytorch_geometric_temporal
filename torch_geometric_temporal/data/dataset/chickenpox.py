ca9a594fb19562b908550260cc9051d69f9d5ae5
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-08 21:37:47 +0000

    PTG

--- torch_geometric_temporal/data/dataset/chickenpox.py
+++ torch_geometric_temporal/data/dataset/chickenpox.py
@@ -11,10 +11,10 @@ class ChickenpoxDatasetLoader(object):
 
     def _read_web_data(self):
         url = "https://raw.githubusercontent.com/benedekrozemberczki/pytorch_geometric_temporal/master/dataset/discrete/chickenpox.json"
-        self.dataset = json.loads(urllib.request.urlopen(url).read())
+        self._dataset = json.loads(urllib.request.urlopen(url).read())
 
     def _get_edges(self):
-        self._edges = np.array(self.dataset["edges"]).T
+        self._edges = np.array(self._dataset["edges"]).T
 
     def _get_edge_weights(self):
         self._edge_weights = np.ones(self._edges.shape[1])
@@ -22,11 +22,12 @@ class ChickenpoxDatasetLoader(object):
     def get_dataset(self):
         self._get_edges()
         self._get_edge_weights()
+
         features = []
         targets = []
-        for time in range(self.dataset["time_periods"]):
-            features.append(np.array(self.dataset[str(time)]["y"]))
-            targets.append(np.array(self.dataset[str(time)]["X"]))
-        return StaticGraphDiscreteSignal(self.edges, edge_weights, features, targets)
+        for time in range(self._dataset["time_periods"]):
+            features.append(np.array(self._dataset[str(time)]["y"]))
+            targets.append(np.array(self._dataset[str(time)]["X"]))
+        return StaticGraphDiscreteSignal(self._edges, self._edge_weights, features, targets)
  
         
