0eef0a670aab181b98bb0a9ccacbcf95a63c1623
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-08 21:51:43 +0000

    PTG

--- test/dataset_test.py
+++ test/dataset_test.py
@@ -4,7 +4,7 @@ from torch_geometric_temporal.data.dataset import ChickenpoxDatasetLoader
 def test_chickenpox():
     loader = ChickenpoxDatasetLoader()
     dataset = loader.get_dataset()
-    assert dataset.edge_index.shape == (2, 102)
-    for epoch in range(10):
+    for epoch in range(3):
         for snapshot in dataset:
-            assert snapshot.edge_index.shape ==(2, 102)
+            assert snapshot.edge_index.shape == (2, 102)
+            assert snapshot.edge_weight.shape == (1, 102)
