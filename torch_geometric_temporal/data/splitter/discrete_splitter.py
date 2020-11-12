803069ff097fd180fcbe315bb923fc547c0be211
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-12 16:19:12 +0000

    PTG Iterator

--- torch_geometric_temporal/data/splitter/discrete_splitter.py
+++ torch_geometric_temporal/data/splitter/discrete_splitter.py
@@ -1,3 +1,4 @@
+from torch_geometric_temporal.data.discrete.static_graph_discrete_signal import StaticGraphDiscreteSignal
 
 
 def discrete_train_test_split(discrete_iterator, train_ratio: int=0.8):
