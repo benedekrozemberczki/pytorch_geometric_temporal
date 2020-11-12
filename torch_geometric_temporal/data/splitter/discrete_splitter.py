6f6591e62e72bcf7a15bd3246b4381265bfc5836
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-12 16:45:23 +0000

    PTG Iterator

--- torch_geometric_temporal/data/splitter/discrete_splitter.py
+++ torch_geometric_temporal/data/splitter/discrete_splitter.py
@@ -5,9 +5,13 @@ from torch_geometric_temporal.data.discrete.static_graph_discrete_signal import 
 def discrete_train_test_split(data_iterator, train_ratio: float=0.8):
 
     train_snapshots = int(train_ratio*len(data_iterator.features))
-    test_snapshots = len(data_iterator.features) - train_snapshots
 
     if type(data_iterator) == StaticGraphDiscreteSignal:
-        train_iterator = StaticGraphDiscreteSignal(data_iterator.edge_index, data_iterator.edge_weight, [None],[None])
-        test_iterator = StaticGraphDiscreteSignal(data_iterator.edge_index, data_iterator.edge_weight, [None],[None])
+        train_iterator = StaticGraphDiscreteSignal(data_iterator.edge_index,
+                                                   data_iterator.edge_weight,
+                                                   data_iterator.features[0:train_snapshots],
+                                                   data_iterator.targets[0:train_snapshots])
+
+        test_iterator = StaticGraphDiscreteSignal(data_iterator.edge_index,
+                                                  data_iterator.edge_weight, [None],[None])
     return 1
