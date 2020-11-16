abbf2fe46d62c690bbb869df8c3f174cd38000c0
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-11-16 11:12:21 +0000

    [DOCS]

--- example.py
+++ example.py
@@ -11,18 +11,18 @@ from torch_geometric_temporal.data.splitter import discrete_train_test_split
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features):
         super(RecurrentGCN, self).__init__()
-        #self.recurrent_1 = DCRNN(node_features, 32, 2)
-        self.linear = torch.nn.Linear(node_features, 1)
+        self.recurrent_1 = DCRNN(node_features, 32, 1)
+        self.linear_1 = torch.nn.Linear(32, 1)
 
     def forward(self, x, edge_index, edge_weight):
-        #h = self.recurrent_1(x, edge_index, edge_weight)
-        #h = F.relu(h)
-        h = self.linear(x)
+        h = self.recurrent_1(x, edge_index, edge_weight)
+        h = F.relu(h)
+        h = self.linear_1(h)
         return h
 
 dataset = ChickenpoxDatasetLoader().get_dataset()
 
-train_dataset, test_dataset = discrete_train_test_split(dataset, train_ratio=0.8)
+train_dataset, test_dataset = discrete_train_test_split(dataset, train_ratio=0.2)
 
 model = RecurrentGCN(node_features = 4)
 
@@ -30,12 +30,11 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 model.train()
 
-for epoch in tqdm(range(2000)):
+for epoch in tqdm(range(300)):
     cost = 0
     for i, snapshot in enumerate(train_dataset):
-        print(snapshot.x)
         out = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)     
-        cost = cost + torch.mean(torch.square(out-snapshot.y))
+        cost = cost + torch.mean((out-snapshot.y)**2)
     cost = cost / (i+1)
     cost.backward()
     optimizer.step()
@@ -51,4 +50,4 @@ for t, snapshot in enumerate(test_dataset):
 y = np.concatenate(y)
 y_hat = np.concatenate(y_hat)
 print(r2_score(y, y_hat))
-print(model.linear.bias)
+#print(model.linear.bias)
