cda24cd6ffb72512a1f39696bc94bbdbdfda7a34
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-02-17 20:14:46 +0000

    PTG

--- torch_geometric_temporal/nn/recurrent/temporalgcn.py
+++ torch_geometric_temporal/nn/recurrent/temporalgcn.py
@@ -10,6 +10,9 @@ class TGCN(torch.nn.Module):
     Args:
         in_channels (int): Number of input features.
         out_channels (int): Number of output features.
+        improved (bool):
+        cached (bool):
+        add_self_loops (bool):
     """
     def __init__(self, in_channels: int, out_channels: int, improved: bool=False,
                  cached: bool=False, add_self_loops: bool=True):
