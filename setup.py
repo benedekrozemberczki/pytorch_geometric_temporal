cdc337a7bb070a98995f3358a4be2a9fe49898b3
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-02-17 19:51:29 +0000

    PTG

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ keywords = ["machine-learning",
 setup(
   name = "torch_geometric_temporal",
   packages = find_packages(),
-  version = "0.0.12",
+  version = "0.0.13",
   license = "MIT",
   description = "A Temporal Extension Library for PyTorch Geometric.",
   author = "Benedek Rozemberczki",
