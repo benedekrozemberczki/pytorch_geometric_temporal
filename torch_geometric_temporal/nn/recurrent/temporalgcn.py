cecfa52c629e8190059c5ac5463122e7d0f94a1d
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-02-17 20:20:10 +0000

    PTG

--- torch_geometric_temporal/nn/recurrent/temporalgcn.py
+++ torch_geometric_temporal/nn/recurrent/temporalgcn.py
@@ -30,7 +30,10 @@ class TGCN(torch.nn.Module):
     def _create_update_gate_parameters_and_layers(self):
 
         self.conv_z = GCNConv(in_channels=self.in_channels,
-                              out_channels=self.out_channels)
+                              out_channels=self.out_channels,
+                              improved=self.improved,
+                              cached=self.cached,
+                              add_self_loops=self.add_self_loops)
                                         
         self.linear_z = torch.nn.Linear(2*self.out_channels,
                                         self.out_channels)
@@ -39,7 +42,10 @@ class TGCN(torch.nn.Module):
     def _create_reset_gate_parameters_and_layers(self):
 
         self.conv_r = GCNConv(in_channels=self.in_channels,
-                              out_channels=self.out_channels)
+                              out_channels=self.out_channels,
+                              improved=self.improved,
+                              cached=self.cached,
+                              add_self_loops=self.add_self_loops)
                                         
         self.linear_r = torch.nn.Linear(2*self.out_channels,
                                         self.out_channels)
@@ -47,7 +53,10 @@ class TGCN(torch.nn.Module):
     def _create_candidate_state_parameters_and_layers(self):
 
         self.conv_h = GCNConv(in_channels=self.in_channels,
-                              out_channels=self.out_channels)
+                              out_channels=self.out_channels,
+                              improved=self.improved,
+                              cached=self.cached,
+                              add_self_loops=self.add_self_loops)
                                         
         self.linear_h = torch.nn.Linear(2*self.out_channels,
                                         self.out_channels)
