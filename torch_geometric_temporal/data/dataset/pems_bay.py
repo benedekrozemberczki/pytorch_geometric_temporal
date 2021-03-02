b60cae79e32c69c4681dc5d2f6b1966e7342eeaf
paulmorio <pms69@cam.ac.uk>
2021-03-02 22:30:06 +0000

    [PEMSBAY] Created raw data and read_web_function with placeholder url

--- torch_geometric_temporal/data/dataset/pems_bay.py
+++ torch_geometric_temporal/data/dataset/pems_bay.py
@@ -11,24 +11,46 @@ class PemsBayDatasetLoader(object):
     """
 
     def __init__(self):
-        super(PemsBayDatasetLoader, self).__init()
+        super(PemsBayDatasetLoader, self).__init__()
         self._read_web_data()
 
-    def _read_web_data():
+    def _read_web_data(self):
         url = "placeholder"
-        pass
+        
+        if (not os.path.isfile("data/PEMS-BAY.zip")):
+            urllib.request.urlopen(url).read()
+
+        if (not os.path.isfile("data/pems_adj_mat.npy") or not os.path.isfile("data/pems_node_values.npy")):
+            with zipfile.ZipFile("data/PEMS-BAY.zip", "r") as zip_fh:
+                zip_fh.extractall("data/")
+
+        A = np.load("data/pems_adj_mat.npy")
+        X = np.load("data/pems_node_values.npy").transpose((1,2,0))
+        X = X.astype(np.float32)
+
+        # Normalise as in DCRNN paper (via Z-Score Method)
+        means = np.mean(X, axis=(0, 2))
+        X = X - means.reshape(1, -1, 1)
+        stds = np.std(X, axis=(0, 2))
+        X = X / stds.reshape(1, -1, 1)
+        
+        self.A = torch.from_numpy(A)
+        self.X = torch.from_numpy(X)
 
     def _get_edges_and_weights(self):
-        pass
+        edge_indices, values = dense_to_sparse(self.A)
+        edge_indices = edge_indices.numpy()
+        values = values.numpy()
+        self.edges = edge_indices
+        self.edge_weights = values
 
     def _generate_task(self, num_timesteps_in: int=12, num_timesteps_out: int=2):
         pass
-
+        
     def get_dataset(self, num_timesteps_in: int=12, num_timesteps_out: int=2):
         pass
 
 if __name__ == '__main__':
-    pass
-
-
-
+    from torch_geometric_temporal.data.discrete.static_graph_discrete_signal import StaticGraphDiscreteSignal
+    loader = PemsBayDatasetLoader()
+    dataset = loader.get_dataset()
