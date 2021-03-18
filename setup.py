9675f2ec58356578617d1f03aa8f18c267ef3084
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-18 23:58:23 +0000

    PTG

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = ["torch",
                     "torch_geometric",
                     "numpy",
                     "networkx",
-                    "scipy",
+                    "scipy==1.5.4",
                     "tqdm",
                     "six"]
 
