128987d1bcc654df397fd45a457562abbaad300d
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-21 12:49:00 +0000

    PTG

--- torch_geometric_temporal/nn/convolutional/astgcn.py
+++ torch_geometric_temporal/nn/convolutional/astgcn.py
@@ -5,20 +5,24 @@ from torch_geometric.transforms import LaplacianLambdaMax
 from torch_geometric.data import Data
 from .chebconvatt import ChebConvAtt
 
-class Spatial_Attention_layer(nn.Module):
+class SpatialAttentionLayer(nn.Module):
     '''
-    compute spatial attention scores
+    Compute spatial attention scores
     '''
-    def __init__(self, in_channels, num_of_vertices, num_of_timesteps):
+    def __init__(self, in_channels: int, num_of_vertices: int, num_of_timesteps: int):
         super(Spatial_Attention_layer, self).__init__()
+        
         self.W1 = nn.Parameter(torch.FloatTensor(num_of_timesteps))
         self.W2 = nn.Parameter(torch.FloatTensor(in_channels, num_of_timesteps))
         self.W3 = nn.Parameter(torch.FloatTensor(in_channels))
+        
         self.bs = nn.Parameter(torch.FloatTensor(1, num_of_vertices, num_of_vertices))
+        
         self.Vs = nn.Parameter(torch.FloatTensor(num_of_vertices, num_of_vertices))
-        self.reset_parameters()
+        
+        self._reset_parameters()
 
-    def reset_parameters(self):
+    def _reset_parameters(self):
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
@@ -26,25 +30,25 @@ class Spatial_Attention_layer(nn.Module):
                 nn.init.uniform_(p)
 
 
-    def forward(self, x):
+    def forward(self, X):
         """
         Making a forward pass of the spatial attention layer.
         B is the batch size. N_nodes is the number of nodes in the graph. F_in is the dimension of input features. 
         T_in is the length of input sequence in time. 
         Arg types:
-            * x (PyTorch Float Tensor) - Node features for T time periods, with shape (B, N_nodes, F_in, T_in).
+            * X (PyTorch Float Tensor) - Node features for T time periods, with shape (B, N_nodes, F_in, T_in).
 
         Return types:
             * output (PyTorch Float Tensor) - Spatial attention score matrices, with shape (B, N_nodes, N_nodes).
         """
 
-        lhs = torch.matmul(torch.matmul(x, self.W1), self.W2)  # (b,N,F,T)(T)->(b,N,F)(F,T)->(b,N,T)
+        LHS = torch.matmul(torch.matmul(X, self.W1), self.W2)  # (b,N,F,T)(T)->(b,N,F)(F,T)->(b,N,T)
 
-        rhs = torch.matmul(self.W3, x).transpose(-1, -2)  # (F)(b,N,F,T)->(b,N,T)->(b,T,N)
+        RHS = torch.matmul(self.W3, X).transpose(-1, -2)  # (F)(b,N,F,T)->(b,N,T)->(b,T,N)
 
-        product = torch.matmul(lhs, rhs)  # (b,N,T)(b,T,N) -> (B, N, N)
+        L_R_product = torch.matmul(LHS, RHS)  # (b,N,T)(b,T,N) -> (B, N, N)
 
-        S = torch.matmul(self.Vs, torch.sigmoid(product + self.bs))  # (N,N)(B, N, N)->(B,N,N)
+        S = torch.matmul(self.Vs, torch.sigmoid(L_R_product + self.bs))  # (N,N)(B, N, N)->(B,N,N)
 
         S_normalized = F.softmax(S, dim=1)
 
@@ -102,7 +106,7 @@ class ASTGCN_block(nn.Module):
     def __init__(self, in_channels, K, nb_chev_filter, nb_time_filter, time_strides, num_of_vertices, num_of_timesteps):
         super(ASTGCN_block, self).__init__()
         self.TAt = Temporal_Attention_layer(in_channels, num_of_vertices, num_of_timesteps)
-        self.SAt = Spatial_Attention_layer(in_channels, num_of_vertices, num_of_timesteps)
+        self.SAt = SpatialAttentionLayer(in_channels, num_of_vertices, num_of_timesteps)
         self.cheb_conv_SAt = ChebConvAtt(in_channels, nb_chev_filter, K)
         self.time_conv = nn.Conv2d(nb_chev_filter, nb_time_filter, kernel_size=(1, 3), stride=(1, time_strides), padding=(0, 1))
         self.residual_conv = nn.Conv2d(in_channels, nb_time_filter, kernel_size=(1, 1), stride=(1, time_strides))
