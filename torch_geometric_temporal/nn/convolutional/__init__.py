7c2e5bacef1cd49e48dfd9028cddb3b3c15464fd
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-21 13:51:02 +0000

    PTG

--- torch_geometric_temporal/nn/convolutional/__init__.py
+++ torch_geometric_temporal/nn/convolutional/__init__.py
@@ -1,7 +1,5 @@
 from .stgcn import STConv, TemporalConv
 from .astgcn import ASTGCN
 from .mstgcn import MSTGCN
-from .chebconvatt import ChebConvAtt
 from .mtgnn import MTGNN
-from .mtgnn_layers import MixProp, GraphConstructor
-from .gman import GMAN, STAttBlock, STEmbedding
+from .gman import GMAN
