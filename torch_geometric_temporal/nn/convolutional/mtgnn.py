f643fad09f8cc0533aef9d3a7521e975f32128cd
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-21 13:49:45 +0000

    PTG

--- torch_geometric_temporal/nn/convolutional/mtgnn.py
+++ torch_geometric_temporal/nn/convolutional/mtgnn.py
@@ -1,5 +1,196 @@
-from .mtgnn_layers import *
+from __future__ import division
+import torch
+import torch.nn as nn
+from torch.nn import init
+import numbers
+import torch.nn.functional as F
 
+
+class nconv(nn.Module):
+    def __init__(self):
+        super(nconv,self).__init__()
+
+    def forward(self,x, A):
+        x = torch.einsum('ncwl,vw->ncvl',(x,A))
+        return x.contiguous()
+
+class linear(nn.Module):
+    def __init__(self,c_in,c_out,bias=True):
+        super(linear,self).__init__()
+        self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 1), padding=(0,0), stride=(1,1), bias=bias)
+
+    def forward(self,x):
+        return self.mlp(x)
+
+
+class MixProp(nn.Module):
+    r"""An implementation of the dynatic mix-hop propagation layer.
+    For details see this paper: `"Connecting the Dots: Multivariate Time Series Forecasting with Graph Neural Networks." <https://arxiv.org/pdf/2005.11650.pdf>`_
+
+    Args:
+        c_in (int) : number of input channels.
+        c_out (int) : number of output channels.
+        gdep (int) : depth of graph convolution.
+        dropout (float) : dropout rate.
+        alpha (float) : ratio of retaining the root nodes's original states, a value between 0 and 1.
+    """
+    def __init__(self,c_in,c_out,gdep,dropout,alpha):
+        super(MixProp, self).__init__()
+        self.nconv = nconv()
+        self.mlp = linear((gdep+1)*c_in,c_out)
+        self.gdep = gdep
+        self.dropout = dropout
+        self.alpha = alpha
+
+
+    def forward(self,x,adj):
+        """
+        Making a forward pass of mix-hop propagation.
+        
+        Arg types:
+            * x (Pytorch Float Tensor): input feature Tensor, with shape (batch_size, c_in, num_nodes, seq_len).
+            * adj (PyTorch Float Tensor) - adjacency matrix, with shape (num_nodes, num_nodes).
+
+        Return types:
+            * ho (PyTorch Float Tensor) - hidden representation for all nodes, with shape (batch_size, c_out, num_nodes, seq_len).
+        """
+        adj = adj + torch.eye(adj.size(0)).to(x.device) # add self-loops
+        d = adj.sum(1)
+        h = x
+        out = [h]
+        a = adj / d.view(-1, 1)
+        for i in range(self.gdep):
+            h = self.alpha*x + (1-self.alpha)*self.nconv(h,a)
+            out.append(h)
+        ho = torch.cat(out,dim=1)
+        ho = self.mlp(ho)
+        return ho
+
+class dilated_inception(nn.Module):
+    def __init__(self, cin, cout, dilation_factor=2):
+        super(dilated_inception, self).__init__()
+        self.tconv = nn.ModuleList()
+        self.kernel_set = [2,3,6,7]
+        cout = int(cout/len(self.kernel_set))
+        for kern in self.kernel_set:
+            self.tconv.append(nn.Conv2d(cin,cout,(1,kern),dilation=(1,dilation_factor)))
+
+    def forward(self,input):
+        x = []
+        for i in range(len(self.kernel_set)):
+            x.append(self.tconv[i](input))
+        for i in range(len(self.kernel_set)):
+            x[i] = x[i][...,-x[-1].size(3):]
+        x = torch.cat(x,dim=1)
+        return x
+
+
+class GraphConstructor(nn.Module):
+    r"""An implementation of the graph learning layer to construct an adjacency matrix.
+    For details see this paper: `"Connecting the Dots: Multivariate Time Series Forecasting with Graph Neural Networks." <https://arxiv.org/pdf/2005.11650.pdf>`_
+
+    Args:
+        nnodes (int) : number of nodes in the graph.
+        k (int) : the number of largest values to consider in constructing the neighbourhood of a node (pick the "nearest" k nodes).
+        dim (int) : dimension of the node embedding.
+        alpha (float, optional) : tanh alpha for generating adjacency matrix, alpha controls the saturation rate, default 3.
+        static_feat (Pytorch Float Tensor, optional) : static feature, default None.
+    """
+    def __init__(self, nnodes, k, dim, alpha=3, static_feat=None):
+        super(GraphConstructor, self).__init__()
+        self.nnodes = nnodes
+        if static_feat is not None:
+            xd = static_feat.shape[1]
+            self.lin1 = nn.Linear(xd, dim)
+            self.lin2 = nn.Linear(xd, dim)
+        else:
+            self.emb1 = nn.Embedding(nnodes, dim)
+            self.emb2 = nn.Embedding(nnodes, dim)
+            self.lin1 = nn.Linear(dim,dim)
+            self.lin2 = nn.Linear(dim,dim)
+
+        self.k = k
+        self.dim = dim
+        self.alpha = alpha
+        self.static_feat = static_feat
+
+    def forward(self, idx):
+        """
+        Making a forward pass to construct an adjacency matrix from node embeddings.
+        
+        Arg types:
+            * idx (Tensor, optional): input indices, a permutation of the number of nodes, default None (no permutation).
+
+        Return types:
+            * adj (PyTorch Float Tensor) - adjacency matrix constructed from node embeddings.
+        """
+        if self.static_feat is None:
+            nodevec1 = self.emb1(idx)
+            nodevec2 = self.emb2(idx)
+        else:
+            nodevec1 = self.static_feat[idx,:]
+            nodevec2 = nodevec1
+
+        nodevec1 = torch.tanh(self.alpha*self.lin1(nodevec1))
+        nodevec2 = torch.tanh(self.alpha*self.lin2(nodevec2))
+
+        a = torch.mm(nodevec1, nodevec2.transpose(1,0))-torch.mm(nodevec2, nodevec1.transpose(1,0))
+        adj = F.relu(torch.tanh(self.alpha*a))
+        mask = torch.zeros(idx.size(0), idx.size(0)).to(adj.device)
+        mask.fill_(float('0'))
+        s1,t1 = adj.topk(self.k,1)
+        mask.scatter_(1,t1,s1.fill_(1))
+        adj = adj*mask
+        return adj
+
+    def fullA(self, idx):
+        if self.static_feat is None:
+            nodevec1 = self.emb1(idx)
+            nodevec2 = self.emb2(idx)
+        else:
+            nodevec1 = self.static_feat[idx,:]
+            nodevec2 = nodevec1
+
+        nodevec1 = torch.tanh(self.alpha*self.lin1(nodevec1))
+        nodevec2 = torch.tanh(self.alpha*self.lin2(nodevec2))
+
+        a = torch.mm(nodevec1, nodevec2.transpose(1,0))-torch.mm(nodevec2, nodevec1.transpose(1,0))
+        adj = F.relu(torch.tanh(self.alpha*a))
+        return adj
+
+
+class LayerNorm(nn.Module):
+    __constants__ = ['normalized_shape', 'weight', 'bias', 'eps', 'elementwise_affine']
+    def __init__(self, normalized_shape, eps=1e-5, elementwise_affine=True):
+        super(LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        self.normalized_shape = tuple(normalized_shape)
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
+        if self.elementwise_affine:
+            self.weight = nn.Parameter(torch.Tensor(*normalized_shape))
+            self.bias = nn.Parameter(torch.Tensor(*normalized_shape))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        if self.elementwise_affine:
+            init.ones_(self.weight)
+            init.zeros_(self.bias)
+
+    def forward(self, input, idx):
+        if self.elementwise_affine:
+            return F.layer_norm(input, tuple(input.shape[1:]), self.weight[:,idx,:], self.bias[:,idx,:], self.eps)
+        else:
+            return F.layer_norm(input, tuple(input.shape[1:]), self.weight, self.bias, self.eps)
+
+    def extra_repr(self):
+        return '{normalized_shape}, eps={eps}, ' \
+            'elementwise_affine={elementwise_affine}'.format(**self.__dict__)
 
 class MTGNN(nn.Module):
     r"""An implementation of the Multivariate Time Series Forecasting Graph Neural Networks.
