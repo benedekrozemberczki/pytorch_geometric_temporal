cccb7dadfd0e7705a5d2bbef6a30405480a44899
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-22 21:37:31 +0000

    PTG update

--- torch_geometric_temporal/data/dataset/wikimath.py
+++ torch_geometric_temporal/data/dataset/wikimath.py
@@ -11,7 +11,7 @@ class WikiMathsDatasetLoader(object):
     links between them. The graph is directed and weighted. Weights represent
     the number of links found at the source Wikipedia page linking to the target
     Wikipedia page. The target is the daily user visits to the Wikipedia pages
-    between march 16. 2019 and march 15. 2021. which results in 731 periods.
+    between March 16th 2019 and March 15th 2021 which results in 731 periods.
     """
     def __init__(self):
         self._read_web_data()
