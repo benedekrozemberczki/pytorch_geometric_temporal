8ef106da9aa581975272bb95886bdd1f7eef7a2a
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-23 23:05:38 +0000

    PTG update

--- torch_geometric_temporal/__init__.py
+++ torch_geometric_temporal/__init__.py
@@ -2,7 +2,7 @@ from torch_geometric_temporal.nn import *
 from torch_geometric_temporal.dataset import *
 from torch_geometric_temporal.signal import *
 
-__version__ = '0.0.22'
+__version__ = '0.0.23'
 
 __all__ = [
     'torch_geometric',
