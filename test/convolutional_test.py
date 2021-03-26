863aafc5286dbd186d0ab3dd3c9925f282275fa5
Yixuan He <He_YX@outlook.com>
2021-03-26 11:06:13 +0000

    hyperparameters added to gman

--- test/convolutional_test.py
+++ test/convolutional_test.py
@@ -337,13 +337,18 @@ def test_gman():
     batch_size = 32
     bn_decay = 0.1
     steps_per_day = 288
+    same_padding = False
+    use_bias = True
     trainX = torch.rand(num_sample,num_his, num_nodes)
     SE, _ = create_mock_data(number_of_nodes=num_nodes, edge_per_node=8, in_channels=64)
     trainTE = 2 * torch.rand((num_sample, num_his + num_pred, 2)) - 1
-    model = GMAN(L, K, d, num_his, bn_decay=bn_decay, steps_per_day=steps_per_day).to(device)
+    model = GMAN(L, K, d, num_his, bn_decay=bn_decay, steps_per_day=steps_per_day, same_padding=same_padding, use_bias=use_bias).to(device)
+    model2 = GMAN(L, K, d, num_his, bn_decay=bn_decay, steps_per_day=steps_per_day, same_padding=same_padding, use_bias=False).to(device)
 
     X = trainX[:batch_size].to(device)
     TE = trainTE[:batch_size].to(device)
     pred = model(X, SE, TE)
+    assert pred.shape == (batch_size, num_pred, num_nodes)
+    pred = model2(X, SE, TE)
     assert pred.shape == (batch_size, num_pred, num_nodes)
 
