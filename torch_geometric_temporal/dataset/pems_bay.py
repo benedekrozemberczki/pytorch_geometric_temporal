730a17815589f39aa187c342804bb391bde8e6b6
paulmorio <pms69@cam.ac.uk>
2021-03-28 19:47:38 +0100

    [Docs] Dynamic network details

--- torch_geometric_temporal/dataset/pems_bay.py
+++ torch_geometric_temporal/dataset/pems_bay.py
@@ -7,7 +7,12 @@ from six.moves import urllib
 from ..signal import StaticGraphTemporalSignal
 
 class PemsBayDatasetLoader(object):
-    """A traffic forecasting dataset as described in Diffusion Convolution Layer Paper. 
+    """A traffic forecasting dataset as described in Diffusion Convolution Layer Paper.
+
+    This traffic dataset is collected by California Transportation Agencies (CalTrans) 
+    Performance Measurement System (PeMS). It is represented by a network of 325 traffic sensors
+    in the Bay Area with 6 months of traffic readings ranging from Jan 1st 2017 to May 31th 2017 
+    in 5 minute intervals.
     
     For details see: `"Diffusion Convolutional Recurrent Neural Network:
     Data-Driven Traffic Forecasting" <https://arxiv.org/abs/1707.01926>`_
