e1b6d87de08b37ab5e8db744d569a16a5a33e07c
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-28 13:56:18 +0100

    PTG

--- test/dataset_test.py
+++ test/dataset_test.py
@@ -4,6 +4,7 @@ import networkx as nx
 from torch_geometric_temporal.signal import temporal_signal_split
 from torch_geometric_temporal.signal import StaticGraphTemporalSignal
 from torch_geometric_temporal.signal import DynamicGraphTemporalSignal
+from torch_geometric_temporal.signal import DynamicGraphStaticSignal
 
 from torch_geometric_temporal.dataset import METRLADatasetLoader, PemsBayDatasetLoader, WindmillOutputDatasetLoader
 from torch_geometric_temporal.dataset import ChickenpoxDatasetLoader, PedalMeDatasetLoader, WikiMathsDatasetLoader
@@ -51,7 +52,7 @@ def test_dynamic_graph_discrete_signal_real():
             assert snapshot.y.shape == (100, )
 
 
-def test_static_graph_discrete_signal():
+def test_static_graph_temporal_signal():
     dataset = StaticGraphTemporalSignal(None, None, [None, None],[None, None])
     for snapshot in dataset:
         assert snapshot.edge_index is None
@@ -59,7 +60,7 @@ def test_static_graph_discrete_signal():
         assert snapshot.x is None
         assert snapshot.y is None
 
-def test_dynamic_graph_discrete_signal():
+def test_dynamic_graph_temporal_signal():
     dataset = DynamicGraphTemporalSignal([None, None], [None, None], [None, None],[None, None])
     for snapshot in dataset:
         assert snapshot.edge_index is None
@@ -67,7 +68,7 @@ def test_dynamic_graph_discrete_signal():
         assert snapshot.x is None
         assert snapshot.y is None
 
-def test_static_graph_discrete_signal_typing():
+def test_static_graph_temporal_signal_typing():
     dataset = StaticGraphTemporalSignal(None, None, [np.array([1])],[np.array([2])])
     for snapshot in dataset:
         assert snapshot.edge_index is None
@@ -186,6 +187,37 @@ def test_discrete_train_test_split_dynamic():
     targets = [np.random.uniform(0,10,(n_count,)) for _ in range(snapshot_count)]
 
     dataset = DynamicGraphTemporalSignal(edge_indices, edge_weights, features, targets)
+
+    train_dataset, test_dataset = temporal_signal_split(dataset, 0.8)
+
+    for epoch in range(2):
+        for snapshot in test_dataset:
+            assert snapshot.edge_index.shape[0] == 2
+            assert snapshot.edge_index.shape[1] == snapshot.edge_attr.shape[0]
+            assert snapshot.x.shape == (100, 32)
+            assert snapshot.y.shape == (100, )
+
+    for epoch in range(2):
+        for snapshot in train_dataset:
+            assert snapshot.edge_index.shape[0] == 2
+            assert snapshot.edge_index.shape[1] == snapshot.edge_attr.shape[0]
+            assert snapshot.x.shape == (100, 32)
+            assert snapshot.y.shape == (100, )
+            
+            
+def test_discrete_train_test_split_dynamic():
+
+    snapshot_count = 250
+    n_count = 100
+    feature_count = 32
+
+    edge_indices, edge_weights, features = generate_signal(250, 100, 32)
+    
+    feature = features[0]
+
+    targets = [np.random.uniform(0,10,(n_count,)) for _ in range(snapshot_count)]
+
+    dataset = DynamicGraphStaticSignal(edge_indices, edge_weights, feature, targets)
 
     train_dataset, test_dataset = temporal_signal_split(dataset, 0.8)
 
