fb07d394df480d16a599fd0c7a182611d09f2d4c
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-03-30 20:40:35 +0100

    PTG

--- torch_geometric_temporal/nn/recurrent/mpnn_lstm.py
+++ torch_geometric_temporal/nn/recurrent/mpnn_lstm.py
@@ -13,21 +13,21 @@ class MPNNLSTM(nn.Module):
         in_channels (int): Number of input features.
         out_channels (int): Number of output features.
         hidden_size (int): Dimension of hidden representations.
-        n_nodes (int): Number of nodes in the network.
+        num_nodes (int): Number of nodes in the network.
         window (int): Number of past samples included in the input.
         dropout (float): Dropout rate.
     """
-    def __init__(self, in_channels: int, hidden_size: int , out_channels: int, n_nodes: int, window: int, dropout: float):
+    def __init__(self, in_channels: int, hidden_size: int , out_channels: int, num_nodes: int, window: int, dropout: float):
         super(MPNNLSTM_, self).__init__()
         self.window = window
-        self.n_nodes = n_nodes
+        self.num_nodes = num_nodes
         self.hidden_size = hidden_size
         self.dropout = dropout
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.create_parameters_and_layers()
+        self._create_parameters_and_layers()
         
-    def create_parameters_and_layers(self):
+    def _create_parameters_and_layers(self):
         self.conv1 = GCNConv(self.in_channels, self.hidden_size)
         self.conv2 = GCNConv(self.hidden_size, self.hidden_size)
         
@@ -43,7 +43,7 @@ class MPNNLSTM(nn.Module):
     def hgcn1(self, x, edge_index, edge_weight):
         x = self.relu(self.conv1(x, edge_index, edge_weight))
         x = self.bn1(x)
-        x = self.dropout(x)
+        x = torch.nn.dropout(x, self.training)
         return x
     
     def hgcn2(self, x, edge_index, edge_weight):
@@ -53,21 +53,22 @@ class MPNNLSTM(nn.Module):
         return x
      
         
-    def forward(self, x ,edge_index, edge_weight):
+    def forward(self, x: torch.FloatTensor, edge_index: torch.LongTensor,
+               edge_weight: torch.FloatTensor) -> torch.FloatTensor:
         """
         Making a forward pass through the whole architecture.
         
         Arg types:
-            * **x** *(PyTorch Float Tensor)* - Node features.
-            * **edge_index** *(PyTorch Long Tensor)* - Graph edge indices.
-            * **edge_weight** *(PyTorch Long Tensor, optional)* - Edge weight vector.
+            * **x** *(PyTorch FloatTensor)* - Node features.
+            * **edge_index** *(PyTorch LongTensor)* - Graph edge indices.
+            * **edge_weight** *(PyTorch LongTensor, optional)* - Edge weight vector.
 
         Return types:
-            *  **H** *(PyTorch Float Tensor)* - The hidden representation of size 2*nhid+2*in_channels-1 for each node.
+            *  **x** *(PyTorch Float Tensor)* - The hidden representation of size 2*nhid+2*in_channels-1 for each node.
         """
         lst = list()
         
-        skip = x.view(-1,self.window,self.n_nodes,self.in_channels)
+        skip = x.view(-1,self.window,self.num_nodes,self.in_channels)
         skip = torch.transpose(skip, 1, 2).reshape(-1,self.window,self.in_channels)
         overlap = [skip[:,0,:]]
         for l in range(1,self.window):
@@ -82,13 +83,13 @@ class MPNNLSTM(nn.Module):
         
         x = torch.cat(lst, dim=1)
 
-        x = x.view(-1, self.window, self.n_nodes, x.size(1))
+        x = x.view(-1, self.window, self.num_nodes, x.size(1))
         x = torch.transpose(x, 0, 1)
         x = x.contiguous().view(self.window, -1, x.size(3))
         
         
         x, (hn1, cn1) = self.rnn1(x)
-        x, (hn2,  cn2) = self.rnn2(x)
+        x, (hn2, cn2) = self.rnn2(x)
         
         x = torch.cat([hn1[0,:,:],hn2[0,:,:],skip], dim=1)
         
