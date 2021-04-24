f5774cd32a8f124caabdb36aa24e76bd6320866a
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-04-24 18:54:22 +0100

    PTGT

--- torch_geometric_temporal/nn/recurrent/agcrn.py
+++ torch_geometric_temporal/nn/recurrent/agcrn.py
@@ -4,46 +4,58 @@ import torch.nn.functional as F
 
 
 class AVWGCN(nn.Module):
-    def __init__(self, dim_in, dim_out, cheb_k, embed_dim):
+    r"""An implementation of the Diffusion Convolution Layer. 
+    For details see: `"Diffusion Convolutional Recurrent Neural Network:
+    Data-Driven Traffic Forecasting" <https://arxiv.org/abs/1707.01926>`_
+
+    Args:
+        in_channels (int): Number of input features.
+        out_channels (int): Number of output features.
+        K (int): Filter size :math:`K`.
+        embedding_dimensions (int): Number of node embedding dimensions.
+    """
+    def __init__(self, in_channels, out_channels, K, embedding_dimensions):
         super(AVWGCN, self).__init__()
-        self.cheb_k = cheb_k
-        self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, dim_in, dim_out))
-        self.bias_pool = nn.Parameter(torch.FloatTensor(embed_dim, dim_out))
-    def forward(self, x, node_embeddings):
-        #x shaped[B, N, C], node_embeddings shaped [N, D] -> supports shaped [N, N]
-        #output shape [B, N, C]
-        node_num = node_embeddings.shape[0]
-        supports = F.softmax(F.relu(torch.mm(node_embeddings, node_embeddings.transpose(0, 1))), dim=1)
+        self.K = K
+        self.weights_pool = nn.Parameter(torch.FloatTensor(embedding_dimensions, K, in_channels, out_channels))
+        self.bias_pool = nn.Parameter(torch.FloatTensor(embedding_dimensions, out_channels))
+        
+    def forward(self, X, E):
+    
+        node_num = E.shape[0]
+        supports = F.softmaX(F.relu(torch.mm(E, E.transpose(0, 1))), dim=1)
         support_set = [torch.eye(node_num).to(supports.device), supports]
-        #default cheb_k = 3
-        for k in range(2, self.cheb_k):
-            support_set.append(torch.matmul(2 * supports, support_set[-1]) - support_set[-2])
+        
+        for _ in range(2, self.K):
+            support = torch.matmul(2 * supports, support_set[-1]) - support_set[-2]
+            support_set.append(support)
+            
         supports = torch.stack(support_set, dim=0)
-        weights = torch.einsum('nd,dkio->nkio', node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
-        bias = torch.matmul(node_embeddings, self.bias_pool)                       #N, dim_out
-        x_g = torch.einsum("knm,bmc->bknc", supports, x)      #B, cheb_k, N, dim_in
-        x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
-        x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
-        return x_gconv
+        W = torch.einsum('nd,dkio->nkio', E, self.weights_pool) 
+        bias = torch.matmul(E, self.bias_pool)
+        X_g = torch.einsum("knm,bmc->bknc", supports, X)
+        X_g = X_g.permute(0, 2, 1, 3)
+        X_gconv = torch.einsum('bnki,nkio->bno', X_g, W) + bias
+        return X_gconv
 
 
 class AGCRN(nn.Module):
-    def __init__(self, node_num, dim_in, dim_out, cheb_k, embed_dim):
+    def __init__(self, node_num, in_channels, out_channels, K, embedding_dimensions):
         super(AGCRN, self).__init__()
         self.node_num = node_num
-        self.hidden_dim = dim_out
-        self.gate = AVWGCN(dim_in+self.hidden_dim, 2*dim_out, cheb_k, embed_dim)
-        self.update = AVWGCN(dim_in+self.hidden_dim, dim_out, cheb_k, embed_dim)
+        self.hidden_dim = out_channels
+        self.gate = AVWGCN(in_channels+self.hidden_dim, 2*out_channels, K, embedding_dimensions)
+        self.update = AVWGCN(in_channels+self.hidden_dim, out_channels, K, embedding_dimensions)
 
-    def forward(self, x, state, node_embeddings):
-        #x: B, num_nodes, input_dim
+    def forward(self, X, state, E):
+        #X: B, num_nodes, input_dim
         #state: B, num_nodes, hidden_dim
-        state = state.to(x.device)
-        input_and_state = torch.cat((x, state), dim=-1)
-        z_r = torch.sigmoid(self.gate(input_and_state, node_embeddings))
+        state = state.to(X.device)
+        input_and_state = torch.cat((X, state), dim=-1)
+        z_r = torch.sigmoid(self.gate(input_and_state, E))
         z, r = torch.split(z_r, self.hidden_dim, dim=-1)
-        candidate = torch.cat((x, z*state), dim=-1)
-        hc = torch.tanh(self.update(candidate, node_embeddings))
+        candidate = torch.cat((X, z*state), dim=-1)
+        hc = torch.tanh(self.update(candidate, E))
         h = r*state + (1-r)*hc
         return h
 
