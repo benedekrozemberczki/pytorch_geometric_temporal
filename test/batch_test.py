1f3d8b7c2eeae7085a06485cb12429d489c2b7af
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-04-25 19:17:10 +0100

    PTGT

--- test/batch_test.py
+++ test/batch_test.py
@@ -7,12 +7,21 @@ from torch_geometric_temporal.signal import StaticGraphTemporalSignalBatch
 from torch_geometric_temporal.signal import DynamicGraphTemporalSignalBatch
 from torch_geometric_temporal.signal import DynamicGraphStaticSignalBatch
 
-def get_edge_array(n_count):
-    return np.array([edge for edge in nx.gnp_random_graph(n_count, 0.1).edges()]).T
+def get_edge_array(node_count):
+    return np.array([edge for edge in nx.gnp_random_graph(node_count, 0.1).edges()]).T
 
-def generate_signal(snapshot_count, n_count, feature_count):
-    edge_indices = [get_edge_array(n_count) for _ in range(snapshot_count)]
+def generate_signal(snapshot_count, node_count, feature_count):
+    edge_indices = [get_edge_array(node_count) for _ in range(snapshot_count)]
     edge_weights = [np.ones(edge_indices[t].shape[1]) for t in range(snapshot_count)]
-    features = [np.random.uniform(0,1,(n_count, feature_count)) for _ in range(snapshot_count)]
+    features = [np.random.uniform(0, 1, (node_count, feature_count)) for _ in range(snapshot_count)]
     return edge_indices, edge_weights, features
+    
+def generate_batch_data(node_count, snapshot_count, feature_count, batch_size)
+    edges = []
+    edge_weights = []
+    features = []
+    targets = []
+    batch_index = []
+    for _ in range(batch_size):
+        x = 2
 
