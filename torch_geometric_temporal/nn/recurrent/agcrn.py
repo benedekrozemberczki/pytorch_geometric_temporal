17b7109b2c72f01362536b1c2db33be6942d9acd
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2021-06-04 15:40:33 +0100

    Update agcrn.py

--- torch_geometric_temporal/nn/recurrent/agcrn.py
+++ torch_geometric_temporal/nn/recurrent/agcrn.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn.inits import glorot, zeros
 
 
 class AVWGCN(nn.Module):
     r"""An implementation of the Node Adaptive Graph Convolution Layer.
     For details see: `"Adaptive Graph Convolutional Recurrent Network
     for Traffic Forecasting" <https://arxiv.org/abs/2007.02842>`_
-
     Args:
         in_channels (int): Number of input features.
         out_channels (int): Number of output features.
@@ -17,20 +17,20 @@ class AVWGCN(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, K: int, embedding_dimensions: int):
         super(AVWGCN, self).__init__()
         self.K = K
-        self.weights_pool = nn.Parameter(torch.FloatTensor(embedding_dimensions, K,
-                                                           in_channels, out_channels))
-        self.bias_pool = nn.Parameter(torch.FloatTensor(embedding_dimensions, out_channels))
+        self.weights_pool = torch.nn.Parameter(torch.Tensor(embedding_dimensions, K, in_channels, out_channels))
+        self.bias_pool = torch.nn.Parameter(torch.Tensor(embedding_dimensions, out_channels))
+        glorot(self.weights_pool)
+        zeros(self.bias_pool)
         
     def forward(self, X: torch.FloatTensor, E: torch.FloatTensor) -> torch.FloatTensor:
         r"""Making a forward pass.
-
         Arg types:
             * **X** (PyTorch Float Tensor) - Node features.
             * **E** (PyTorch Float Tensor) - Node embeddings.
-
         Return types:
-            * **E** (PyTorch Float Tensor) - Hidden state matrix for all nodes.
+            * **X_G** (PyTorch Float Tensor) - Hidden state matrix for all nodes.
         """
+
         number_of_nodes = E.shape[0]
         supports = F.softmax(F.relu(torch.mm(E, E.transpose(0, 1))), dim=1)
         support_set = [torch.eye(number_of_nodes).to(supports.device), supports]
@@ -40,24 +40,22 @@ class AVWGCN(nn.Module):
         supports = torch.stack(support_set, dim=0)
         W = torch.einsum('nd,dkio->nkio', E, self.weights_pool) 
         bias = torch.matmul(E, self.bias_pool)
-        X = torch.einsum("knm,bmc->bknc", supports, X)
-        X = X.permute(0, 2, 1, 3)
-        X = torch.einsum('bnki,nkio->bno', X, W) + bias
-        return X
+        X_G = torch.einsum("knm,bmc->bknc", supports, X)
+        X_G = X_G.permute(0, 2, 1, 3)
+        X_G = torch.einsum('bnki,nkio->bno', X_G, W) + bias
+        return X_G
 
 
 class AGCRN(nn.Module):
     r"""An implementation of the Adaptive Graph Convolutional Recurrent Unit.
     For details see: `"Adaptive Graph Convolutional Recurrent Network
     for Traffic Forecasting" <https://arxiv.org/abs/2007.02842>`_
-
     Args:
         number_of_nodes (int): Number of vertices.
         in_channels (int): Number of input features.
         out_channels (int): Number of output features.
         K (int): Filter size :math:`K`.
         embedding_dimensions (int): Number of node embedding dimensions.
-
     """
     def __init__(self, number_of_nodes: int, in_channels: int,
                  out_channels: int, K: int, embedding_dimensions: int):
@@ -88,12 +86,10 @@ class AGCRN(nn.Module):
 
     def forward(self, X: torch.FloatTensor, E: torch.FloatTensor, H: torch.FloatTensor=None)  -> torch.FloatTensor:
         r"""Making a forward pass.
-
         Arg types:
             * **X** (PyTorch Float Tensor) - Node feature matrix.
             * **H** (PyTorch Float Tensor) - Node hidden state matrix. Default is None.
             * **E** (PyTorch Float Tensor) - Node embedding matrix.
-
         Return types:
             * **H** (PyTorch Float Tensor) - Hidden state matrix for all nodes.
         """       
