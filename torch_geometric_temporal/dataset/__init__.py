e47875a75c0a2ba81d6426a89dc2b0c5e5fcf05a
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-06-12 14:39:38 +0100

    PTGT

--- torch_geometric_temporal/dataset/__init__.py
+++ torch_geometric_temporal/dataset/__init__.py
@@ -4,6 +4,8 @@ from .metr_la import METRLADatasetLoader
 from .pems_bay import PemsBayDatasetLoader
 from .wikimath import WikiMathsDatasetLoader
 from .windmilllarge import WindmillOutputLargeDatasetLoader
+from .windmillmedium import WindmillOutputMediumDatasetLoader
+from .windmillsmall import WindmillOutputSmallDatasetLoader
 from .encovid import EnglandCovidDatasetLoader
 from .twitter_tennis import TwitterTennisDatasetLoader
 from .montevideo_bus import MontevideoBusDatasetLoader
