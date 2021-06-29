0e50f3885139e9d95aa678cc76c35d637a52f735
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-06-29 22:56:07 +0100

    TGCN Example

---
+++ examples/recurrent/gclstm_example.py
@@ -0,0 +1,52 @@
+from tqdm import tqdm
+
+import torch
+import torch.nn.functional as F
+from torch_geometric_temporal.nn.recurrent import GCLSTM
+
+from torch_geometric_temporal.dataset import ChickenpoxDatasetLoader
+from torch_geometric_temporal.signal import temporal_signal_split
+
+loader = ChickenpoxDatasetLoader()
+
+dataset = loader.get_dataset()
+
+train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
+
+class RecurrentGCN(torch.nn.Module):
+    def __init__(self, node_features):
+        super(RecurrentGCN, self).__init__()
+        self.recurrent = GCLSTM(node_features, 32, 1)
+        self.linear = torch.nn.Linear(32, 1)
+
+    def forward(self, x, edge_index, edge_weight, h, c):
+        h_0, c_0 = self.recurrent(x, edge_index, edge_weight, h, c)
+        h = F.relu(h_0)
+        h = self.linear(h)
+        return h, h_0, c_0
+        
+model = RecurrentGCN(node_features=4)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+model.train()
+
+for epoch in tqdm(range(200)):
+    cost = 0
+    h, c = None, None
+    for time, snapshot in enumerate(train_dataset):
+        y_hat, h, c = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr, h, c)
+        cost = cost + torch.mean((y_hat-snapshot.y)**2)
+    cost = cost / (time+1)
+    cost.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    
+model.eval()
+cost = 0
+for time, snapshot in enumerate(test_dataset):
+    y_hat, h, c = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr, h, c)
+    cost = cost + torch.mean((y_hat-snapshot.y)**2)
+cost = cost / (time+1)
+cost = cost.item()
+print("MSE: {:.4f}".format(cost))
