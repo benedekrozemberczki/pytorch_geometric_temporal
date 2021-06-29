c82c1f630441695bb79206778caaa9613216b719
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-06-29 22:39:50 +0100

    TGCN Example

--- examples/gconvlstm_example.py
+++ examples/gconvlstm_example.py
@@ -19,11 +19,11 @@ class RecurrentGCN(torch.nn.Module):
         self.recurrent = GConvLSTM(node_features, 32, 1)
         self.linear = torch.nn.Linear(32, 1)
 
-    def forward(self, x, edge_index, edge_weight):
-        h = self.recurrent(x, edge_index, edge_weight)
-        h = F.relu(h)
+    def forward(self, x, edge_index, edge_weight, h, c):
+        h_0, c_0 = self.recurrent(x, edge_index, edge_weight, h, c)
+        h = F.relu(h_0)
         h = self.linear(h)
-        return h
+        return h, h_0, c_0
         
 model = RecurrentGCN(node_features = 4)
 
@@ -33,8 +33,9 @@ model.train()
 
 for epoch in tqdm(range(200)):
     cost = 0
+    h, c = None, None
     for time, snapshot in enumerate(train_dataset):
-        y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+        y_hat, h, c = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr, h, c)
         cost = cost + torch.mean((y_hat-snapshot.y)**2)
     cost = cost / (time+1)
     cost.backward()
