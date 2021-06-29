e6c44320b39f329f9afb1cca5c81c46bc6912783
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-06-29 23:44:17 +0100

    TGCN Example

--- examples/recurrent/mpnnlstm_example.py
+++ examples/recurrent/mpnnlstm_example.py
@@ -16,8 +16,8 @@ train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features):
         super(RecurrentGCN, self).__init__()
-        self.recurrent = MPNNLSTM(node_features, 32, 20, 0.5)
-        self.linear = torch.nn.Linear(32, 1)
+        self.recurrent = MPNNLSTM(node_features, 32, 32, 20, 1, 0.5)
+        self.linear = torch.nn.Linear(2*32 + node_features, 1)
 
     def forward(self, x, edge_index, edge_weight):
         h = self.recurrent(x, edge_index, edge_weight)
