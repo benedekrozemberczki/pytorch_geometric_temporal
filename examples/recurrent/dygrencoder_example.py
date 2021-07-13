28518b37272222ca4fd3bbcce70b28cfff5cfaee
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-07-13 22:00:50 +0100

    Disjoint resolve docs

--- examples/recurrent/dygrencoder_example.py
+++ examples/recurrent/dygrencoder_example.py
@@ -16,7 +16,7 @@ train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features):
         super(RecurrentGCN, self).__init__()
-        self.recurrent = LRGCN(node_features, 32, 1, 1)
+        self.recurrent = LRGCN(conv_out_channels=4, conv_num_layers=1 conv_aggr="mean", lstm_out_channels=32, lstm_num_layers=1)
         self.linear = torch.nn.Linear(32, 1)
 
     def forward(self, x, edge_index, edge_weight, h_0, c_0):
