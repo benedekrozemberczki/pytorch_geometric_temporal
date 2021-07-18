280cbf68b022164c657ac557e9bb95d5f627cab1
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-07-18 20:37:56 +0100

    DNNTSP

--- torch_geometric_temporal/nn/attention/dnntsp.py
+++ torch_geometric_temporal/nn/attention/dnntsp.py
@@ -6,7 +6,7 @@ from torch.autograd import Variable
 from torch_geometric.utils.to_dense_adj import to_dense_adj
 import torch.nn.functional as F
 
-class DNNTSP(object):
+class DNNTSP(nn.Module):
 
      def __init__(self):
          self.num = 32
