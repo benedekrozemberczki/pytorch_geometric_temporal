b26c5d7541addcf892a8bdb17c8644e0c54a78aa
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-07-18 20:41:38 +0100

    DNNTSP

--- torch_geometric_temporal/nn/attention/dnntsp.py
+++ torch_geometric_temporal/nn/attention/dnntsp.py
@@ -6,6 +6,47 @@ from torch.autograd import Variable
 from torch_geometric.utils.to_dense_adj import to_dense_adj
 import torch.nn.functional as F
 
+
+class GlobalGatedUpdater(nn.Module):
+
+    def __init__(self, items_total, item_embedding):
+        super(global_gated_update, self).__init__()
+        self.items_total = items_total
+        self.item_embedding = item_embedding
+
+        # alpha -> the weight for updating
+        self.alpha = nn.Parameter(torch.rand(items_total, 1), requires_grad=True)
+
+    def forward(self, graph, nodes, nodes_output):
+        """
+        :param graph: batched graphs, with the total number of nodes is `node_num`,
+                        including `batch_size` disconnected subgraphs
+        :param nodes: tensor (n_1+n_2+..., )
+        :param nodes_output: the output of self-attention model in time dimension, (n_1+n_2+..., F)
+        :return:
+        """
+        nums_nodes, id = graph.batch_num_nodes(), 0
+        items_embedding = self.item_embedding(torch.tensor([i for i in range(self.items_total)]).to(nodes.device))
+        batch_embedding = []
+        for num_nodes in nums_nodes:
+            # tensor, shape, (user_nodes, item_embed_dim)
+            output_node_features = nodes_output[id:id + num_nodes, :]
+            # get each user's nodes
+            output_nodes = nodes[id: id + num_nodes]
+            # beta, tensor, (items_total, 1), indicator vector, appear item -> 1, not appear -> 0
+            beta = torch.zeros(self.items_total, 1).to(nodes.device)
+            beta[output_nodes] = 1
+            # update global embedding by gated mechanism
+            # broadcast (items_total, 1) * (items_total, item_embed_dim) -> (items_total, item_embed_dim)
+            embed = (1 - beta * self.alpha) * items_embedding.clone()
+            # appear items: (1 - self.alpha) * origin + self.alpha * update, not appear items: origin
+            embed[output_nodes, :] = embed[output_nodes, :] + self.alpha[output_nodes] * output_node_features
+            batch_embedding.append(embed)
+            id += num_nodes
+        # (B, items_total, item_embed_dim)
+        batch_embedding = torch.stack(batch_embedding)
+        return batch_embedding
+ 
 class DNNTSP(nn.Module):
 
 
@@ -31,8 +72,8 @@ class DNNTSP(nn.Module):
 
         self.aggregate_nodes_temporal_feature = aggregate_nodes_temporal_feature(item_embed_dim=item_embedding_dim)
 
-        self.global_gated_update = global_gated_update(items_total=items_total,
-                                                       item_embedding=self.item_embedding)
+        self.global_gated_update = GlobalGatedUpdater(items_total=items_total,
+                                                      item_embedding=self.item_embedding)
 
         self.fc_output = nn.Linear(item_embedding_dim, 1)
 
@@ -63,3 +104,4 @@ class DNNTSP(nn.Module):
         output = self.fc_output(nodes_output).squeeze(dim=-1)
 
         return output
+ 
