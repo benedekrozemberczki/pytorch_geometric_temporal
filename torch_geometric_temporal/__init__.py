a45aa6e96081789058e751a450928a440e8476b2
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-07-25 11:39:16 +0100

    DNNTSPO

--- torch_geometric_temporal/__init__.py
+++ torch_geometric_temporal/__init__.py
@@ -2,7 +2,7 @@ from torch_geometric_temporal.nn import *
 from torch_geometric_temporal.dataset import *
 from torch_geometric_temporal.signal import *
 
-__version__ = '0.0.38'
+__version__ = '0.39'
 
 __all__ = [
     'torch_geometric',
