57f4422a4ad4dd78a06813b87d8bc58786de5e11
Gregor Donabauer <gregor.donabauer@gmx.de>
2022-01-10 09:20:12 +0100

    Updated torch_geometric version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
     "torch_scatter",
     "torch_cluster",
     "torch_spline_conv",
-    "torch_geometric==1.7.0",
+    "torch_geometric==2.0.0",
     "numpy",
     "scipy",
     "tqdm",
