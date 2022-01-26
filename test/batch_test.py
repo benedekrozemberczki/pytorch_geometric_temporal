74b18d076e4114412b04a1427b4916e2065b067c
Gregor Donabauer <gregor.donabauer@gmx.de>
2022-01-26 10:10:58 +0100

    Added batch tests for coverage
    edge test
    edge weights test
    additional attributes None test
    batch assigned test

--- test/batch_test.py
+++ test/batch_test.py
@@ -163,11 +163,13 @@ def test_static_graph_temporal_signal_batch_additional_attrs():
 def test_static_hetero_graph_temporal_signal_batch_additional_attrs():
     dataset = StaticHeteroGraphTemporalSignalBatch(None, None, [None], [None], None,
                                                    optional1=[{'author': np.array([1])}],
-                                                   optional2=[{'author': np.array([2])}])
-    assert dataset.additional_feature_keys == ["optional1", "optional2"]
+                                                   optional2=[{'author': np.array([2])}],
+                                                   optional3=[None])
+    assert dataset.additional_feature_keys == ["optional1", "optional2", "optional3"]
     for snapshot in dataset:
         assert snapshot.node_stores[0]['optional1'].shape == (1,)
         assert snapshot.node_stores[0]['optional2'].shape == (1,)
+        assert "optional3" not in list(dict(snapshot.node_stores[0]).keys())
 
 
 def test_dynamic_graph_static_signal_batch_additional_attrs():
@@ -177,6 +179,33 @@ def test_dynamic_graph_static_signal_batch_additional_attrs():
     for snapshot in dataset:
         assert snapshot.optional1.shape == (1,)
         assert snapshot.optional2.shape == (1,)
+
+
+def test_static_hetero_graph_temporal_signal_batch_edges():
+    dataset = StaticHeteroGraphTemporalSignalBatch({("author", "writes", "paper"): np.array([[0, 1], [1, 0]])},
+                                                   {("author", "writes", "paper"): np.array([[0.1], [0.1]])},
+                                                   [{"author": np.array([[0], [0]]),
+                                                     "paper": np.array([[0], [0], [0]])},
+                                                    {"author": np.array([[0.1], [0.1]]),
+                                                     "paper": np.array([[0.1], [0.1], [0.1]])}],
+                                                   [None, None],
+                                                   None)
+    for snapshot in dataset:
+        assert snapshot.edge_stores[0]['edge_index'].shape == (2, 2)
+        assert snapshot.edge_stores[0]['edge_attr'].shape == (2, 1)
+        assert snapshot.edge_stores[0]['edge_index'].shape[0] == snapshot.edge_stores[0]['edge_attr'].shape[0]
+
+
+def test_static_hetero_graph_temporal_signal_batch_assigned():
+    dataset = StaticHeteroGraphTemporalSignalBatch(
+        None, None, [{'author': np.array([1])}], [{'author': np.array([2])}], {'author': np.array([1])}
+    )
+    for snapshot in dataset:
+        assert snapshot.node_types[0] == 'author'
+        assert snapshot.node_stores[0]['x'].shape == (1,)
+        assert snapshot.node_stores[0]['y'].shape == (1,)
+        assert snapshot.node_stores[0]['batch'].shape == (1,)
+        assert len(snapshot.edge_types) == 0
 
 
 def test_discrete_train_test_split_dynamic_batch():
