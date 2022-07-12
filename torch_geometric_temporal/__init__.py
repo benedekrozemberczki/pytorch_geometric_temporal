15bb16a71f0549b6192fe19a27fb40d98ec12aa6
Rozemberczki <kmdb028@astrazeneca.net>
2022-07-12 17:33:27 +0100

    PYG

--- torch_geometric_temporal/__init__.py
+++ torch_geometric_temporal/__init__.py
@@ -2,7 +2,7 @@ from torch_geometric_temporal.nn import *
 from torch_geometric_temporal.dataset import *
 from torch_geometric_temporal.signal import *
 
-__version__ = "0.51.0"
+__version__ = "0.53.0"
 
 __all__ = [
     "torch_geometric",
