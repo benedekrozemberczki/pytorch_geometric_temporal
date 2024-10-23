e5776fbcf7d6c69122edc4a09d0e8aff2e43e0b4
yuxuan-z <yuxuan-z19@qq.com>
2024-10-23 23:13:29 +0800

    fix weight in-place update

--- examples/recurrent/evolvegcnh_example.py
+++ examples/recurrent/evolvegcnh_example.py
@@ -23,11 +23,11 @@ class RecurrentGCN(torch.nn.Module):
         self.recurrent = EvolveGCNH(node_count, node_features)
         self.linear = torch.nn.Linear(node_features, 1)
 
-    def forward(self, x, edge_index, edge_weight):
-        h = self.recurrent(x, edge_index, edge_weight)
+    def forward(self, x, edge_index, edge_weight, w):
+        h, w = self.recurrent(x, edge_index, edge_weight, w)
         h = F.relu(h)
         h = self.linear(h)
-        return h
+        return h, w
         
 model = RecurrentGCN(node_features = 4, node_count = 20)
 
@@ -37,8 +37,9 @@ model.train()
 
 for epoch in tqdm(range(200)):
     cost = 0
+    w = None
     for time, snapshot in enumerate(train_dataset):
-        y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+        y_hat, w = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr, w)
         cost = cost + torch.mean((y_hat-snapshot.y)**2)
     cost = cost / (time+1)
     cost.backward()
@@ -48,7 +49,7 @@ for epoch in tqdm(range(200)):
 model.eval()
 cost = 0
 for time, snapshot in enumerate(test_dataset):
-    y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+    y_hat, w = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr, w)
     cost = cost + torch.mean((y_hat-snapshot.y)**2)
 cost = cost / (time+1)
 cost = cost.item()
