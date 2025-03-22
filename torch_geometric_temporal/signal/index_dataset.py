4b11d0d4497e5c754850fedd12536df34d4d1661
Seth Ockerman <sockerman@wisc.edu>
2025-03-22 12:01:10 -0500

    Add docs to include index dataset and update comments to match formatting

--- torch_geometric_temporal/signal/index_dataset.py
+++ torch_geometric_temporal/signal/index_dataset.py
@@ -7,8 +7,8 @@ import numpy as np
 
 class IndexDataset(Dataset):
     """
-    A custom PyTorch Dataset that implements indexBatching and lazyIndexBatching.
-    It also supports intergration with GPU indexPreprocessing and lazyPreprocessing.
+    A custom pytorch-compatible dataset that implements index-batching and DDP-index-batching.
+    It also supports GPU-index-batching and lazy-index-batching.
 
     Args:
             indices (array-like): Indices corresponding to the time slicies.
@@ -17,7 +17,7 @@ class IndexDataset(Dataset):
             lazy (bool, optional): Whether to use Dask lazy loading (distribute the data across all workers). Defaults to False.
             gpu (bool, optional): If the data is already on the GPU. Defaults to False.
     """
-    def __init__(self,indices, data, horizon, lazy=False, gpu=False):
+    def __init__(self, indices, data, horizon, lazy=False, gpu=False):
          self.indices = indices 
          self.data = data
          self.horizon = horizon
@@ -25,6 +25,7 @@ class IndexDataset(Dataset):
          self.gpu = gpu
         
     def __len__(self):
+
         # Return the number of samples
         return self.indices.shape[0]
 
@@ -41,15 +42,15 @@ class IndexDataset(Dataset):
 
         idx = self.indices[x]
         
-        # calcuate the offset based on the horizon value
+        # Calculate the offset based on the horizon value
         y_start = idx + self.horizon
 
-        # if the data is already on the gpu (likely due to using index-gpu-preprocessing), return tensor-slice
+        # If the data is already on the gpu (likely due to using index-gpu-preprocessing), return tensor-slice
         if self.gpu:
             return self.data[idx:y_start,...], self.data[y_start:y_start + self.horizon,...]
         
         else:
-            # if utilizing lazyBatching, gather the data on to this worker and convert to tensor
+            # if utilizing DDP-batching, gather the data on to this worker and convert to tensor
             if self.lazy:
                 return torch.from_numpy(self.data[idx:y_start,...].compute()),torch.from_numpy(self.data[y_start:y_start + self.horizon,...].compute())
             else:
